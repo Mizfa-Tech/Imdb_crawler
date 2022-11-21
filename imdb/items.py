5042139fec79bb8ceceb0e4a843154b8631a57a4
Mohammadreza <mohammadrezasolo69@gmail.com>
2022-11-21 09:16:23 -0800

    write item and write loader

--- imdb/items.py
+++ imdb/items.py
@@ -4,9 +4,25 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders import processors
+
+def run_time(value:list[str]):
+    hours , minutes = value[0],value[4]
+    return f"{hours} hours {minutes} minutes"
 
 
 class ImdbItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    imdb_id = scrapy.Field(output_processor=processors.TakeFirst())
+    title = scrapy.Field(output_processor=processors.TakeFirst())
+    release_date = scrapy.Field(output_processor=processors.TakeFirst())
+    cover_url = scrapy.Field(output_processor=processors.TakeFirst())
+    genres = scrapy.Field(output_processor=processors.Identity())
+    description = scrapy.Field(output_processor=processors.TakeFirst())
+    director = scrapy.Field(output_processor=processors.Identity())
+    writers = scrapy.Field(output_processor=processors.Identity())
+    country_of_origin = scrapy.Field(output_processor=processors.TakeFirst())
+    language = scrapy.Field(output_processor=processors.Identity())
+    run_time = scrapy.Field(input_processor=processors.Compose(run_time),output_processor=processors.TakeFirst())
+    rate = scrapy.Field(output_processor=processors.TakeFirst())
+    rete_population = scrapy.Field(output_processor=processors.TakeFirst())
+    casts = scrapy.Field(output_processor=processors.Identity())
--- imdb/spiders/imdb_single.py
+++ imdb/spiders/imdb_single.py
@@ -1,5 +1,7 @@
 import scrapy
 import re
+from scrapy.loader import ItemLoader
+from imdb.items import ImdbItem
 
 
 class ImdbSpider(scrapy.Spider):
@@ -9,34 +11,73 @@ class ImdbSpider(scrapy.Spider):
     ]
     
     def parse(self, response, **kwargs):
-        
-        imdb_id = re.findall("tt\d{7}",response.url)['-1']
-        title = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/h1/text()').get()
-        release_date = response.xpath('/html/body/div[2]/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/div/ul/li[1]/a/text()').get()
-        cover_url = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[1]/div/div[1]/div/div[1]/img/@src').getall()
-        genres = response.css('span.ipc-chip__text::text').getall()
-        description = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[1]/p/span[1]/text()').get()
-        creator = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[3]/ul/li[1]/div/ul/li/a/text()').get()
-        country_of_origin = response.css('section[data-testid="Details"] li[data-testid="title-details-origin"] a::text').get()
-        
-        language = response.css('section[data-testid="Details"] li[data-testid="title-details-languages"] a::text').get()
-        run_time = response.css('section[data-testid="TechSpecs"] li[data-testid="title-techspec_runtime"] div::text').getall()
+        loader = ItemLoader(item=ImdbItem(),response=response)
         
         
-        rate = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[2]/div/div[1]/a/div/div/div[2]/div[1]/span[1]/text()').get()
-        rete_population = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[2]/div/div[1]/a/div/div/div[2]/div[3]/text()').get()
+        imdb_id = re.findall("tt\d{7}",response.url)[0]
+        loader.add_value('imdb_id',imdb_id)
+    
+    
+        # title = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/h1/text()').get()
+        # title = response.css('h1[data-testid="hero-title-block__title"]::text').get()
+        loader.add_css('title','h1[data-testid="hero-title-block__title"]::text')
         
+        
+        # # release_date = response.xpath('/html/body/div[2]/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/div/ul/li[1]/a/text()').get()
+        # # release_date = response.css('li[data-testid="title-details-releasedate"] div a::text').get()
+        loader.add_css('release_date','li[data-testid="title-details-releasedate"] div a::text')
+        
+        
+        # # cover_url = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[1]/div/div[1]/div/div[1]/img/@src').getall()
+        # # cover_url = response.css('div[data-testid="hero-media__poster"] div.ipc-media img::attr(src)').get()
+        loader.add_css('cover_url','div[data-testid="hero-media__poster"] div.ipc-media img::attr(src)')
+
+        
+        # # genres = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[1]/div[1]/div[2]/a[1]/span/text()').getall()
+        # # genres = response.css('div[data-testid="genres"] div.ipc-chip-list__scroller a span::text').getall()
+        loader.add_css('genres','div[data-testid="genres"] div.ipc-chip-list__scroller a span::text')
+        
+        
+        # # description = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[1]/p/span[1]/text()').get()
+        # # description = response.css('span[data-testid="plot-xl"]::text').get()
+        loader.add_css('description','span[data-testid="plot-xl"]::text')
+        
+        # # director = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[3]/ul/li[1]/div/ul/li/a/text()').get()
+        # # director = response.css('li[data-testid="title-pc-principal-credit"] div a::text').get()
+        loader.add_xpath('director','//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[4]/ul/li[1]/div/ul/li/a/text()')
+        
+        loader.add_xpath('writers','//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[4]/ul/li[2]/div/ul/li/a/text()')
+      
+      
+        # # country_of_origin = response.css('section[data-testid="Details"] li[data-testid="title-details-origin"] a::text').get()
+        loader.add_css('country_of_origin','section[data-testid="Details"] li[data-testid="title-details-origin"] a::text')
+        
+        
+        # # language = response.css('section[data-testid="Details"] li[data-testid="title-details-languages"] a::text').get()
+        loader.add_css('language','section[data-testid="Details"] li[data-testid="title-details-languages"] a::text')
+        
+        
+        # # run_time = response.css('section[data-testid="TechSpecs"] li[data-testid="title-techspec_runtime"] div::text').getall()
+        loader.add_css('run_time','section[data-testid="TechSpecs"] li[data-testid="title-techspec_runtime"] div::text')
+        
+        # # rate = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[2]/div/div[1]/a/div/div/div[2]/div[1]/span[1]/text()').get()
+        loader.add_xpath('rate','//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[2]/div/div[1]/a/div/div/div[2]/div[1]/span[1]/text()')
+        
+        # # rete_population = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[2]/div/div[1]/a/div/div/div[2]/div[3]/text()').get()
+        loader.add_xpath('rete_population','//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[2]/div/div[1]/a/div/div/div[2]/div[3]/text()')
         
         casts = response.css('section[data-testid="title-cast"] div[data-testid="shoveler"] div[data-testid="shoveler-items-container"] div[data-testid="title-cast-item"] a::text').getall()
         casts_charcter = response.css('a[data-testid="cast-item-characters-link"] span::text').getall()
+        
         fanal_casts = dict(zip(casts,casts_charcter))
+        loader.add_value('casts',fanal_casts)
+       
+        
+        # yield {
+        #     'imdb_id':imdb_id,'title':title,"language":language,'casts':fanal_casts,'release_date':release_date,
+        #     'cover_url':cover_url,'genres':genres,'description':description,'writers':writers,'director':director,
+        #     'country_of_origin':country_of_origin,"run_time":run_time,'rate':rate,'rete_population':rete_population
+        #     }
         
         
-        
-        yield {
-            'imdb_id':imdb_id,'title':title,"language":language,'casts':fanal_casts,'release_date':release_date,
-            'cover_url':cover_url,'genres':genres,'description':description,'creator':creator,
-            'country_of_origin':country_of_origin,"run_time":run_time,'rate':rate,'rete_population':rete_population
-            }
-        
-        
+        yield loader.load_item()
