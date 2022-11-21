3d6c979655c5b267be102ffa191df271c41a9479
alixo <fromiran98@gmail.com>
2022-11-21 14:45:45 +0330

    update imdb spider

--- imdb/spiders/imdb_single.py
+++ imdb/spiders/imdb_single.py
@@ -8,15 +8,15 @@ class ImdbSpider(scrapy.Spider):
     
     def parse(self, response, **kwargs):
         title = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/h1/text()').get()
-        release_date = response.xpath('//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[12]/div[2]/ul/li[1]/div/ul/li/a/text()').get()
+        release_date = response.xpath('/html/body/div[2]/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/div/ul/li[1]/a/text()').get()
         cover_url = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[1]/div/div[1]/div/div[1]/img/@src').getall()
         genres = response.css('span.ipc-chip__text::text').getall()
         description = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[1]/p/span[1]/text()').get()
         creator = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[3]/ul/li[1]/div/ul/li/a/text()').get()
         country_of_origin = response.xpath('//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[12]/div[2]/ul/li[2]/div/ul/li/a/text()').get()
         casts = response.css('a.sc-bfec09a1-1::text').getall()
-        language = response.xpath('//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[12]/div[2]/ul/li[4]/div/ul/li/a/text()').get()
-        story__line = response.xpath('//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[7]/div[2]/div[1]/div/div/text()').get()
+        language = response.css('section[data-testid="Details"] li[data-testid="title-details-languages"] a::text').get()
+        story__line = response.css('section[data-cel-widget="StaticFeature_Storyline"]').get()
         
         
         yield {
